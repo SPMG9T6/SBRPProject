4b4619ec153ccaeb1325462748558c4ce04f5fb6
jlyr <joachimlimyouren@gmail.com>
2023-10-30 21:42:50 +0800

    added various tests

--- test_unit.py
+++ test_unit.py
@@ -58,7 +58,91 @@ class TestApp(unittest.TestCase):
             assert url_for('request') == '/request'
             assert url_for('redirect') == '/redirect'
 
-           
+
+# Staff View Tests
+
+    def test_view_employee(self):
+        tester = app.test_client(self)
+        response = tester.get('/view_employee', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'View Employee' in response.data)    
+
+# Staff Browse Testing
+
+    def test_browse_employee(self):
+        tester = app.test_client(self)
+        response = tester.get('/browse_employee', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Browse Employee' in response.data)
+
+
+# Staff Filter roles Testing
+
+    def test_filter_roles(self):
+        tester = app.test_client(self)
+        response = tester.get('/filter_roles', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Filter Roles' in response.data)
+
+# Staff Filter department Testing
+
+    def test_filter_department(self):
+        tester = app.test_client(self)
+        response = tester.get('/filter_department', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Filter Department' in response.data)
+
+# Staff Apply Testing
+
+    def test_apply(self):
+        tester = app.test_client(self)
+        response = tester.get('/apply', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Apply' in response.data)
+
+
+# Hr View Tests
+
+    def test_view_hr(self):
+        tester = app.test_client(self)
+        response = tester.get('/view_hr', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'View HR' in response.data)
+
+# HR CRU TESTS - Create, Read, Update
+
+# HR Create Tests
+    def test_create_hr(self):
+        tester = app.test_client(self)
+        response = tester.get('/create_hr', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Create HR' in response.data)
+
+# HR Read Tests
+
+    def test_read_hr(self):
+
+        tester = app.test_client(self)
+        response = tester.get('/read_hr', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Read HR' in response.data)
+
+# HR Update Tests
+
+    def test_update_hr(self):
+
+        tester = app.test_client(self)
+        response = tester.get('/update_hr', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Update HR' in response.data)
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
